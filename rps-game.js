74bfc8f46007eabd36f42e0d06796a531c45d25a
Elijah Jones <elijah21jones@gmail.com>
2022-10-04 17:12:39 -0400

    updated console.log functions to make sure computerSelection was working properly

--- rps-game.js
+++ rps-game.js
@@ -1,24 +1,42 @@
 function getComputerChoice() {
-   const randomNum = Math.floor(Math.random() * 3) 
-switch (randomNum) {
-    case 0:
-        return "Rock";
-    case 1:
-        return "Paper";
-    case 2:
-        return "Scissors";
-}
-}
-console.log(getComputerChoice());
+    const randomNum = Math.floor(Math.random() * 3) /*Generate random num with Math.random(), to ensure it's between 0 and 3, multiply by 3. Use Math.floor to round to down to nearest num */
+ switch (randomNum) { /*Create switch statement to take randomNum variable to perform different action based on the condition (case) */
+     case 0:
+         return "rock";
+     case 1:
+         return "paper";
+     case 2:
+         return "scissors";
+ }
+ }
+
+function userInput() {
+   const ask = prompt("To begin game, select Rock, Paper, or Scissors");
+    return ask.toLowerCase();
+    
+ }
 
 function playRound(playerSelection, computerSelection) {
-
-     playerSelection = prompt("To play game, select either Rock, Paper, or Scissors").toLowerCase();
-
-    if (playerSelection == "Rock" && computerSelection == "Scissors"){
-        return "You win!";
-     } else if (playerSelection == "Rock" && computerSelection == "Paper") {
-        return "You lose";
-     } else if 
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+         return "You win!";
+     } else if (playerSelection == "rock" && computerSelection == "paper") {
+         return "You lose";
+     } else if (playerSelection == "scissors" && computerSelection == "paper") {
+         return "You win!";
+     } else if (playerSelection == "scissors" && computerSelection == "rock") {
+         return "You lose";
+     } else if (playerSelection == "paper" && computerSelection == "rock") {
+         return "You win!";
+     } else if (playerSelection == "paper" && computerSelection == "scissors") {
+         return "You lose";
+     } else if (playerSelection ===  computerSelection) {
+         return "It's a tie";
+     } else {
+         return "Try again";
      }
-}
+ }
+
+    const playerSelection = userInput();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playerSelection, computerSelection)
